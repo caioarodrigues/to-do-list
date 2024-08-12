acced03a7494a5b65fa63b86f67ab893742fee68
Caioã <caioarodrigues@alu.ufc.br>
2024-08-12 18:10:56 -0300

    chore: a few adjustments

--- interface/src/App.tsx
+++ interface/src/App.tsx
@@ -1,29 +1,26 @@
 import { useState, useEffect } from "react";
-import { Task } from "./components/Task";
+import { TaskList } from "./components/TaskList";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:3000/task/list-all")
-      .then((response) => {
-        console.log(response.data);
-      })
+      .then(({ data }) => setTasks(data))
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <Task
-        title="Learn React"
-        description="Learn how to use React"
-        completed={false}
-      />
+      <div>
+        <h1>Tasks</h1>
+        <TaskList tasks={tasks} />
+      </div>
     </>
   );
 }
