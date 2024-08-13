003432e293b284163207e13d723d45a8ba2f0f3d
Caioã <caioarodrigues@alu.ufc.br>
2024-08-13 16:14:12 -0300

    chore: a few adjustments

--- interface/src/components/TaskList.tsx
+++ interface/src/components/TaskList.tsx
@@ -6,27 +6,79 @@ interface TaskListProps {
 }
 
 export const TaskList = ({ tasks }: TaskListProps) => {
+  const doneTasks = tasks.filter((task) => task.completed);
+  const pendingTasks = tasks.filter((task) => !task.completed);
+
   return (
-    <div className="flex flex-col gap-2 w-full md:flex-row md:flex-grow-0 md:flex-wrap
-    md:grid md:grid-cols-4 pt-2">
-      <h2 className="text-4xl font-medium md:hidden md:col-span-1">List of all tasks</h2>
-      {tasks.map(
-        (
-          { id, title, description, completed, dueDate, createdAt, updatedAt },
-          index
-        ) => (
-          <Task
-            key={index}
-            id={id}
-            title={title}
-            description={description}
-            completed={completed}
-            dueDate={dueDate}
-            createdAt={createdAt}
-            updatedAt={updatedAt}
-          />
-        )
-      )}
+    <div>
+      <div>
+        <h2 className="text-4xl font-medium">
+          List of all tasks
+        </h2>
+        <div
+          className="flex flex-col gap-2 w-full md:flex-row md:flex-grow-0 md:flex-wrap
+        md:grid md:grid-cols-4 pt-2"
+        >
+          {pendingTasks.map(
+            (
+              {
+                id,
+                title,
+                description,
+                completed,
+                dueDate,
+                createdAt,
+                updatedAt,
+              },
+              index
+            ) => (
+              <Task
+                key={index}
+                id={id}
+                title={title}
+                description={description}
+                completed={completed}
+                dueDate={dueDate}
+                createdAt={createdAt}
+                updatedAt={updatedAt}
+              />
+            )
+          )}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-4xl font-medium">Done tasks</h2>
+        <div
+          className="flex flex-col gap-2 w-full md:flex-row md:flex-grow-0 md:flex-wrap
+        md:grid md:grid-cols-4 pt-2"
+        >
+          {doneTasks.map(
+            (
+              {
+                id,
+                title,
+                description,
+                completed,
+                dueDate,
+                createdAt,
+                updatedAt,
+              },
+              index
+            ) => (
+              <Task
+                key={index}
+                id={id}
+                title={title}
+                description={description}
+                completed={completed}
+                dueDate={dueDate}
+                createdAt={createdAt}
+                updatedAt={updatedAt}
+              />
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
