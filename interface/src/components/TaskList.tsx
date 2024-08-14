4fff66e73a2c31c0d99bd996098dce062e7246e2
Caioã <caioarodrigues@alu.ufc.br>
2024-08-14 10:33:32 -0300

    feat: sorting tasks by due date

--- interface/src/components/TaskList.tsx
+++ interface/src/components/TaskList.tsx
@@ -7,18 +7,18 @@ interface TaskListProps {
 }
 
 export const TaskList = ({ taskList }: TaskListProps) => {
+  const sortedTaskList = taskList.sort((a, b) => a.dueDate > b.dueDate ? 1 : -1);
   const { tasks, setTasks } = useGlobalContext();
-  setTasks(taskList);
-  
+
+  setTasks(sortedTaskList);
+
   const doneTasks = tasks.filter((task) => task.completed);
   const pendingTasks = tasks.filter((task) => !task.completed);
 
   return (
     <div>
       <div>
-        <h2 className="text-4xl font-medium">
-          List of all tasks
-        </h2>
+        <h2 className="text-4xl font-medium">List of all tasks</h2>
         <div
           className="flex flex-col gap-2 w-full md:flex-row md:flex-grow-0 md:flex-wrap
         md:grid md:grid-cols-4 pt-2"
